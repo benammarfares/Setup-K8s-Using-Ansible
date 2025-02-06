cd1ce022d06c19b2fc296444ac194b6a303b70dc
Fares <99650831+benammarfares@users.noreply.github.com>
2025-02-06 15:26:36 +0100

    Update Script-Master-Nb2.sh

--- Script-Master-Nb2.sh
+++ Script-Master-Nb2.sh
@@ -3,7 +3,6 @@
 # Add the necessary lines to /etc/ssh/sshd_config
 sudo sh -c 'echo "PasswordAuthentication no" >> /etc/ssh/sshd_config'
 sudo sh -c 'echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config'
-sudo sh -c 'echo "PermitRootLogin no" >> /etc/ssh/sshd_config'
 
 # Restart the SSH service to apply changes
 sudo systemctl restart ssh
